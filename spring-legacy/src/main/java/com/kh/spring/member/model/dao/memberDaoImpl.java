2b03547d40dd96f5db4bdeb8ff5db418e6ad01f0
DESKTOP-OQJMVF2\user1 <sim8191si@naver.com>
2026-02-27 17:10:22 +0900

    스프링 2일차

--- spring-legacy/src/main/java/com/kh/spring/member/model/dao/memberDaoImpl.java
+++ spring-legacy/src/main/java/com/kh/spring/member/model/dao/memberDaoImpl.java
@@ -1,8 +1,39 @@
 package com.kh.spring.member.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.kh.spring.member.model.vo.Member;
 
 @Repository
 public class memberDaoImpl implements memberDao{
+
+	@Autowired
+	private SqlSessionTemplate session;
+
+	@Override
+	public Member loginMember(Member m) {
+		// TODO Auto-generated method stub
+		return session.selectOne("member.loginMember", m);
+	}
+
+	@Override
+	public int insertMember(Member m) {
+		// TODO Auto-generated method stub
+		return session.insert("member.insertMember", m);
+	}
+
+	@Override
+	public int idCheck(String userId) {
+		// TODO Auto-generated method stub
+		return session.selectOne("member.idCheck", userId);
+	}
+
+	@Override
+	public Member selectOne(String userId) {
+		// TODO Auto-generated method stub
+		return session.selectOne("member.selectOne", userId);
+	}
 	
 }
--- spring-legacy/src/main/java/com/kh/spring/member/model/service/MemberServiceImpl.java
+++ spring-legacy/src/main/java/com/kh/spring/member/model/service/MemberServiceImpl.java
@@ -1,8 +1,40 @@
 package com.kh.spring.member.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kh.spring.member.model.dao.memberDao;
+import com.kh.spring.member.model.vo.Member;
 
 @Service
 public class MemberServiceImpl implements MemberService{
 
+	@Autowired
+	private memberDao memberDao;
+	
+	@Override
+	public Member loginMember(Member m) {
+		// TODO Auto-generated method stub
+		return memberDao.loginMember(m); 
+	}
+
+	@Override
+	public int insertMember(Member m) {
+		// TODO Auto-generated method stub
+		return memberDao.insertMember(m);
+	}
+
+	@Override
+	public int idCheck(String userId) {
+		// TODO Auto-generated method stub
+		return memberDao.idCheck(userId);
+	}
+
+	@Override
+	public Member selectOne(String userId) {
+		// TODO Auto-generated method stub
+		return memberDao.selectOne(userId);
+	}
+	
+	
 }
