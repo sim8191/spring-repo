694b932a0b67e009a0ffe1c426a0b4f906c900d8
DESKTOP-OQJMVF2\user1 <sim8191si@naver.com>
2026-03-05 15:49:21 +0900

    t

--- Spring공부용계정.sql
+++ Spring공부용계정.sql
@@ -4,5 +4,26 @@ CREATE TABLE AUTHORITIES(
     PRIMARY KEY(USER_NO, AUTHORITY)
 );
 
-INSERT INTO authorities VALUES(11, 'ROLE_ADMIN');
-INSERT INTO authorities VALUES(11, 'ROLE_USER');
+INSERT INTO authorities VALUES(10, 'ROLE_ADMIN');
+INSERT INTO authorities VALUES(10, 'ROLE_USER');
+
+COMMIT;
+--------------
+CREATE TABLE persistent_logins (
+                username        VARCHAR(64)  NOT NULL,
+                series          VARCHAR(64)  PRIMARY KEY,
+                token           VARCHAR(64)  NOT NULL,
+                last_used       TIMESTAMP    NOT NULL
+            );
+
+            CREATE INDEX idx_persistent_logins_username
+            ON persistent_logins(username);
+--------------
+SELECT BOARD_NO,
+			BOARD_TITLE, USER_NAME AS BOARD_WRITER,
+			COUNT,
+			CREATE_DATE
+		FROM BOARD B
+		LEFT JOIN MEMBER M ON BOARD_WRITER = USER_NO
+        WHERE B.STATUS = 'Y' AND BOARD_CD = 'N'
+        ORDER BY BOARD_NO DESC
